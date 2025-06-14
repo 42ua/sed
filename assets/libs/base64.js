551f9a58272be2ba73da913dda3a8934960bc7f4
Oleh Mazko <mazko.private@gmail.com>
2025-06-14 17:10:12 +0300

    github repo instead gist

--- assets/libs/base64.js
+++ assets/libs/base64.js
@@ -64,39 +64,112 @@ $(function() {
     });
   })();
 
-  /* Gist API */
+  /* Github API */
 
   (function() {
     $("li a.gist-api").click(function() {
+
+      var owner = "Zamko84";
+      var repo = "snippets.sed.js";
+      var branch = "main";
+      var token = atob('QmVhcmVyIGdpdGh1Yl9wYXRfMTFCVFNDS05ZMGxhcmFWYUN0UUhlR19wSUZoYUpPUlU' +
+                       '0VkhPck5mR3h5MlI4ekpsVzd0Z3B4Wm00d1hLZVY3dmRpSjJHVldWS1hxR1p0a1Bwbg==');
+
+      var files = [
+        { name: "stdin", content: $("#sed-base64-stdin").val() },
+        { name: "stdin_", content: $("#sed-stdin").val() },
+        { name: "stdout", content: $("#sed-stdout").val() },
+        { name: "args", content: $("#sed-cmd").val() }
+      ];
+
+      var dirID = Math.random().toString(36).substr(2);
+
+      var commitSha;
+
       $.ajax({
         cache: false,
-        contentType: false,
-        processData: false,
-        type: "POST",
-        url: 'https://api.github.com/gists',
+        type: "GET",
+        url: `https://api.github.com/repos/${owner}/${repo}/git/refs/heads/${branch}`,
         headers: {
           'Accept': 'application/vnd.github+json',
           'X-GitHub-Api-Version': '2022-11-28',
-          'Authorization': atob(
-            'QmVhcmVyIGdpdGh1Yl9wYXRfMTFCVFJJNENBMEpuWDhTUVJVV000ZF9oemxrOERISTh' +
-            'sNzJvSEhvdHBPeWRXMWV6cE5yQ3V4SDJsanljUWFucVR5UkxYNERGVUY0ZHpseUYxUQ==')
-        },
-        data: JSON.stringify({
-          "description": "base64sed.js",
-          "public": false,
-          "files": {
-            "stdin": {"content": $("#sed-base64-stdin").val()},
-            "stdin_": {"content": $("#sed-stdin").val()},
-            "stdout": {"content": $("#sed-stdout").val()},
-            "args": {"content": $("#sed-cmd").val()}
+          'Authorization': token
+        }
+      }).then(function(refResponse) {
+        commitSha = refResponse.object.sha;
+        return $.ajax({
+          cache: false,
+          type: "GET",
+          url: `https://api.github.com/repos/${owner}/${repo}/git/commits/${commitSha}`,
+          headers: {
+            'Accept': 'application/vnd.github+json',
+            'X-GitHub-Api-Version': '2022-11-28',
+            'Authorization': token
           }
-        })
+        });
+      }).then(function(commitResponse) {
+        var treeSha = commitResponse.tree.sha;
+        var tree = files.map(function(file) {
+          return {
+            path: `base64sed/${dirID}/${file.name}`,
+            mode: "100644",
+            type: "blob",
+            content: file.content
+          };
+        });
+        return $.ajax({
+          type: "POST",
+          url: `https://api.github.com/repos/${owner}/${repo}/git/trees`,
+          headers: {
+            'Accept': 'application/vnd.github+json',
+            'X-GitHub-Api-Version': '2022-11-28',
+            'Authorization': token
+          },
+          contentType: 'application/json',
+          data: JSON.stringify({
+            base_tree: treeSha,
+            tree: tree
+          })
+        });
+      }).then(function(treeResponse) {
+        var newTreeSha = treeResponse.sha;
+        return $.ajax({
+          type: "POST",
+          url: `https://api.github.com/repos/${owner}/${repo}/git/commits`,
+          headers: {
+            'Accept': 'application/vnd.github+json',
+            'X-GitHub-Api-Version': '2022-11-28',
+            'Authorization': token
+          },
+          contentType: 'application/json',
+          data: JSON.stringify({
+            message: "Update files",
+            tree: newTreeSha,
+            parents: [commitSha]
+          })
+        });
+      }).then(function(commitResponse) {
+        var newCommitSha = commitResponse.sha;
+        return $.ajax({
+          type: "PATCH",
+          url: `https://api.github.com/repos/${owner}/${repo}/git/refs/heads/${branch}`,
+          headers: {
+            'Accept': 'application/vnd.github+json',
+            'X-GitHub-Api-Version': '2022-11-28',
+            'Authorization': token
+          },
+          contentType: 'application/json',
+          data: JSON.stringify({
+            sha: newCommitSha
+          })
+        });
       }).done(function(response) {
-        var url = response.html_url,
-            my = $(location).attr('href').replace(/(#|\?).*$/, "") + '?gist=' + response.id;
+        var sha = response.object.sha,
+            url = `https://github.com/${owner}/${repo}/commit/${sha}`,
+            my = $(location).attr('href').replace(/(#|\?).*$/, "") + '?gh=' + dirID;
         $(".user-errors-here").append( "<div class='alert alert-success alert-dismissible fade in' role=alert>" + 
           "<button type=button class=close data-dismiss=alert aria-label=Close><span aria-hidden=true>&times;</span></button>" + 
-          "<strong>GIST:</strong> <a href='" + url + "'>" + response.id + "</a> | " + 
+          "<strong>COMMIT:</strong> <a href='" + url + "'>" + sha + "</a> | " + 
           "<strong>Share:</strong> <a href='" + my + "'>me</a>" + 
           "</div>"
         );
@@ -116,28 +189,30 @@ $(function() {
 (function() {
   var params    = new URLSearchParams(window.location.search),
       gistId    = params.get('gist') || '14de45cc31aeb042f51aa9fe2ecf00be',
-      snippetId = params.get('snippet'),
+      dirID     = params.get('gh'),
       doc_ready = $.Deferred();
 
   /* http://stackoverflow.com/q/10326398 */
 
   $(doc_ready.resolve);
 
-  if (gistId) {
+  if (dirID) {
+    var baseUrl = 'https://raw.githubusercontent.com/Zamko84/snippets.sed.js/main/base64sed/' + dirID;
+    var content = $.when(
+      $.get( baseUrl + '/args'),
+      $.get( baseUrl + '/stdin'),
+      doc_ready )
+    .then(function( data_args, data_stdin ) {
+      return { args: data_args[0], stdin: data_stdin[0] };
+    });
+  }
+  else {
     var content = $.when(
       $.get( 'https://api.github.com/gists/' + gistId),
       doc_ready )
     .then(function( data ) {
       try { return { args: data[0].files.args.content, stdin: data[0].files.stdin.content }; }
       catch(e) { return $.Deferred().reject({ statusText: 'invalid gist format', status: -1 }); }
-    });
-  } else {
-    var content = $.when(
-      $.get( 'https://api.bitbucket.org/2.0/snippets/sedjs/' + snippetId + '/files/cmd'),
-      $.get( 'https://api.bitbucket.org/2.0/snippets/sedjs/' + snippetId + '/files/base64stdin'),
-      doc_ready )
-    .then(function( data_args, data_stdin ) {
-      return { args: data_args[0], stdin: data_stdin[0] };
     });
   }
 
